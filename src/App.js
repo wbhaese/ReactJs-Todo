80c739b455225ac33ec66d14f6bc6074bcac487d
unknown <wbhaese@gmail.com>
2022-03-19 11:08:53 +0000

    Saving more progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [tasks, setTasks] = useState([
+      {
+        id: '1',
+        title: 'Estudar Dev',
+        completed: false
+      },
+      {
+        id: '2',
+        title: 'Ler Livros',
+        completed: true
+      },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <div className="container"></div>
+    </>
+  ) 
+};
 
 export default App;
