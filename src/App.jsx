d57df24f8c142d656a77f01b973c413b462f2591
WallaceBH <46716156+wbhaese@users.noreply.github.com>
2022-06-08 19:05:21 +0100

    Saving progress
    Saving progress throw 01:05:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,69 @@
 import React, { useState } from "react";
-import Tasks from "./components/Tasks";
+import { v4 as uuidv4 } from "uuid";
 
-import './App.css';
 import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import Header from "./components/Header";
 
+import "./App.css";
 
 const App = () => {
-
   const [tasks, setTasks] = useState([
-      {
-        id: '1',
-        title: 'Estudar Dev',
-        completed: false
-      },
-      {
-        id: '2',
-        title: 'Ler Livros',
-        completed: true
-      },
+    {
+      id: "1",
+      title: "Estudar Dev",
+      completed: false
+    },
+    {
+      id: "2",
+      title: "Ler Livros",
+      completed: true
+    }
   ]);
 
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+
+      return task;
+    });
+
+    setTasks(newTasks);
+  };
+
   const handleTaskAddition = (taskTitle) => {
-    const newTask = [ ... tasks, {
-      title: taskTitle,
-      id: Math.randon(10),
-      completed: false,
-    }]
-  }
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        id: uuidv4(),
+        completed: false
+      }
+    ];
+
+    setTasks(newTasks);
+  };
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(newTasks);
+  };
 
   return (
     <div>
       <div className="container">
-        <AddTask />
-        <Tasks tasks={tasks}/>
+        <Header>Minhas Tarefas</Header>
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks
+          tasks={tasks}
+          handleTaskClick={handleTaskClick}
+          handleTaskDeletion={handleTaskDeletion}
+        />
       </div>
-
     </div>
-  ) 
+  );
 };
 
 export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = ({ children }) => {
+  return <h1>{children}</h1>;
+};
+
+export default Header;
